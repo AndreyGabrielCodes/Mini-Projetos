5221ad5499660797c91a0f33ca8eabc540f27a57
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2024-08-23 12:46:20 -0300

    Update

---
+++ CaçaPalavras-Python.py
@@ -0,0 +1,22 @@
+lista_palavras = ['casa', 'apartamento', 'quarto', 'cozinha', 'sala', 'banheiro', 'janela', 'porta', 'sofa', 'cama', 'mesa', 'cadeira', 'armario', 'geladeira', 'fogao', 'televisao', 'computador','smartphone', 'aplicativo', 'internet', 'wifi', 'algoritmo', 'software', 'hardware', 'nuvem', 'pixel', 'bot','receita', 'ingrediente', 'cozinhar', 'assado', 'frito', 'doce', 'salgado', 'temperos', 'gourmet', 'chef','passaporte', 'mala', 'aviao', 'hotel', 'turismo', 'roteiro', 'paisagem', 'cultura', 'salsicao', 'aventura','futebol', 'basquete', 'volei', 'natacao', 'ginastica', 'atleta', 'torcida', 'estadio', 'olimpiadas', 'medalha']
+partida = {'tamanho_grade':[0,0], 'grade':[], 'palavras_escolhidas':[]}
+
+#caça palavras em que o jogador escolhe o tamanho dela e a partir disso são inseridas palavras dentro
+
+from os import system
+
+def main():
+    system('cls')
+    print('         CAÇA PALAVRAS DINAMICO\n\nExemplo: Tamanho = 3 - Grade de 3x3\n')
+    tamanho_grade = input('Insira o tamanho da grade:')
+
+def cria_posicoes(tamanho_grade):
+    print
+
+def insere_palavras():
+    print
+
+def grade():
+    print
+
+main()
